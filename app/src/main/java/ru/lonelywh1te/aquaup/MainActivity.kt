38b14f3d1fabdc9b810665f12c7b78958e29f697
lonelywh1te <lonelywh1te@ya.ru>
2025-07-29 15:32:40 +1000

    init(*): First commit

---
+++ app/src/main/java/ru/lonelywh1te/aquaup/MainActivity.kt
@@ -0,0 +1,24 @@
+package ru.lonelywh1te.aquaup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.lonelywh1te.aquaup.ui.theme.AquaUpTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
